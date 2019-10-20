1ec2babf2e1756a8e3921661cebf8d4a7f4cdd68
ShibaInuOfMars <1051858676@qq.com>
2019-10-21 00:03:10 +0800

    热门搜索数据获取

--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,9 +1,32 @@
 import * as actionTypes from './actionTypes';
 
+import {fromJS} from 'immutable';
+
+import axios from 'axios';
+
+// 搜索框获得焦点
 export const searchFocus = () => ({
     type: actionTypes.SEARCH_FOCUS
 });
 
+// 搜索框失去焦点
 export const searchBlur = () => ({
     type: actionTypes.SEARCH_BLUR
 });
+
+// 设置热门搜索列表数据
+const setPopularSearchList = (data) => ({
+    type: actionTypes.POPULAR_SEARCH_LIST,
+    data: fromJS(data) // 将普通的js对象转成immutable对象
+});
+
+// 异步获取热门搜索列表数据
+export const getPopularSearchList = () => {
+    return async (dispatch) => {
+        let res = await axios.get('/api/popularSearchList.json');
+        let data = res.data;
+        if(data.success_code === 200) {
+            dispatch(setPopularSearchList(data.result));
+        }
+    };
+};
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -5,18 +5,26 @@ import {fromJS} from 'immutable';
 
 // 通过fromJS方法将一个普通的js对象包装成immutable对象
 const defaultState = fromJS({
-    focused: false
+    focused: false,
+    popularSearchList: [] // 注意：整个对象是经过fromJS包装后的immutable对象，所以该数组也是一个immutable对象，所以不能赋值为普通的js对象，必须同样是immutable对象才能赋值
 });
 
 export default (state = defaultState, action) => {
 
+    // 搜索框获得焦点
     if(action.type === actionTypes.SEARCH_FOCUS) {
         // immutable对象的set方法，会结合之前immutable对象的值，以及现在要设置的值，返回一个全新的对象
         return state.set('focused', true);
     }
 
+    // 搜索框失去焦点
     if(action.type === actionTypes.SEARCH_BLUR) {
         return state.set('focused', false);
+    }
+
+    // 设置热门搜索数据
+    if(action.type === actionTypes.POPULAR_SEARCH_LIST) {
+        return state.set('popularSearchList', action.data);
     }
 
     return state;
