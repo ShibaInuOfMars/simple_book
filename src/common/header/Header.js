30f4bd92c55ec76410bb348ab7c6e9f9e7ef43d8
ShibaInuOfMars <1051858676@qq.com>
2019-10-21 13:31:17 +0800

    头部样式完善

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 import {connect} from 'react-redux';
 
@@ -27,56 +27,54 @@ import {
 const getPopularSearch = (flag) => {
     if(flag) {
         return (
-            <Fragment>
-                <PopularSearch>
-                    <PopularSearchTitle>
-                        热门搜索
-                        <PopularSearchSitch>
-                            换一批
-                        </PopularSearchSitch>
-                    </PopularSearchTitle>
-                    <PopularSearchList>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                        <PopularSearchItem>html</PopularSearchItem>
-                    </PopularSearchList>
-                </PopularSearch>
-                <HeaderWrapper>
-                    <HeaderContent>
-                        <HeaderLogo />
-                        <HeaderToolBtn className="write"><span className="iconfont">&#xe61b;</span>写文章</HeaderToolBtn>
-                        <HeaderToolBtn className="sign-up">注册</HeaderToolBtn>
-                        <HeaderCenter>
-                            <HeaderNav className="nav-left actived"><span className="iconfont">&#xe786;</span>首页</HeaderNav>
-                            <HeaderNav className="nav-left app"><span className="iconfont">&#xe600;</span>下载App</HeaderNav>
-                            <HeaderNav className="nav-right">登录</HeaderNav>
-                            <HeaderNav className="nav-right"><span className="iconfont">&#xe636;</span></HeaderNav>
-                            <HeaderSearchWrapper>
-                                <CSSTransition
-                                    in={focused}
-                                    timeout={200}
-                                    classNames="slide"
-                                >
-                                    <HeaderSearch 
-                                        className={focused ? 'focused' : ''} 
-                                        onFocus={this.handleInputFocus}
-                                        onBlur={this.handleInputBlur}
-                                    />
-                                </CSSTransition> 
-                                <span className={focused ? 'focused iconfont' : 'iconfont'}>&#xe615;</span>
-                            </HeaderSearchWrapper>
-                        </HeaderCenter>
-                    </HeaderContent>
-                </HeaderWrapper>
-            </Fragment>
+            <PopularSearch>
+                <PopularSearchTitle>
+                    热门搜索
+                    <PopularSearchSitch>
+                        换一批
+                    </PopularSearchSitch>
+                </PopularSearchTitle>
+                <PopularSearchList>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                    <PopularSearchItem>html</PopularSearchItem>
+                </PopularSearchList>
+            </PopularSearch>
+            <HeaderWrapper>
+                <HeaderContent>
+                    <HeaderLogo />
+                    <HeaderToolBtn className="write"><span className="iconfont">&#xe61b;</span>写文章</HeaderToolBtn>
+                    <HeaderToolBtn className="sign-up">注册</HeaderToolBtn>
+                    <HeaderCenter>
+                        <HeaderNav className="nav-left actived"><span className="iconfont">&#xe786;</span>首页</HeaderNav>
+                        <HeaderNav className="nav-left app"><span className="iconfont">&#xe600;</span>下载App</HeaderNav>
+                        <HeaderNav className="nav-right">登录</HeaderNav>
+                        <HeaderNav className="nav-right"><span className="iconfont">&#xe636;</span></HeaderNav>
+                        <HeaderSearchWrapper>
+                            <CSSTransition
+                                in={focused}
+                                timeout={200}
+                                classNames="slide"
+                            >
+                                <HeaderSearch 
+                                    className={focused ? 'focused' : ''} 
+                                    onFocus={this.handleInputFocus}
+                                    onBlur={this.handleInputBlur}
+                                />
+                            </CSSTransition> 
+                            <span className={focused ? 'focused iconfont' : 'iconfont'}>&#xe615;</span>
+                        </HeaderSearchWrapper>
+                    </HeaderCenter>
+                </HeaderContent>
+            </HeaderWrapper>
         );
     } else {
         return null;
